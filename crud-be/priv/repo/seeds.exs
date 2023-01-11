77769635e6e237d517cbedee10528ac1b2e2ffd9
Paul Le <taulnle@gmail.com>
2023-01-12 01:00:58 +1100

    Fix seeds.exs

--- crud-be/priv/repo/seeds.exs
+++ crud-be/priv/repo/seeds.exs
@@ -10,5 +10,5 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-Crud.Repo.insert!(%Crud.User{email: "", first_name: "John", last_name: "Doe", mobile: "1234567890", company_profile_id: "Yahoo", title: "CEO"})
-Crud.Repo.insert!(%Crud.User{email: "", first_name: "Barack", last_name: "Obama", mobile: "1234567890", company_profile_id: "USA", title: "Ex-CEO"})
+Crud.Repo.insert!(%Crud.Accounts.User{email: "", first_name: "John", last_name: "Doe", mobile: "1234567890", company_profile_id: "Yahoo", title: "CEO"})
+Crud.Repo.insert!(%Crud.Accounts.User{email: "", first_name: "Barack", last_name: "Obama", mobile: "1234567890", company_profile_id: "USA", title: "Ex-CEO"})
